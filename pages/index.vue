b64df019ef618e51b588036f1e9a3c64cd30d70f
DanielSalis <danielsalis98@gmail.com>
2022-08-13 19:38:54 -0300

    feat: format and add image

--- pages/index.vue
+++ pages/index.vue
@@ -5,18 +5,15 @@
       <v-text-field label="Destino" solo></v-text-field>
       <v-select :items="['Cid 01', 'Cid 02']" label="Cid" solo></v-select>
       <div class="search-page__time-container">
-        <v-text-field
-          label="Horário"
-          prepend-icon="mdi-clock"
-        >
-        </v-text-field>
-        <v-text-field
-          label="Data"
-          prepend-icon="mdi-calendar"
-        >
-        </v-text-field>
+        <v-text-field label="Horário" prepend-icon="mdi-clock"> </v-text-field>
+        <v-text-field label="Data" prepend-icon="mdi-calendar"> </v-text-field>
       </div>
     </div>
+    <img
+      class="search-page__image"
+      src="~/assets/images/woman-hitchhiking.jpg"
+      alt="woman-hitchhiking"
+    />
   </div>
 </template>
 
@@ -28,15 +25,23 @@ export default {
 
 <style lang="scss" scoped>
 .search-page {
+  align-items: center;
   display: flex;
   flex-direction: row;
-  align-items: center;
+  justify-content: space-between;
 }
 
 .search-page__form {
   display: flex;
   flex-direction: column;
+  margin-left: 96px;
+  margin-right: 96px;
   width: 300px;
+}
+
+.search-page__image {
+  max-width: 1000px;
+  object-fit: cover;
 }
 
 .search-page__time-container {
