bd19d5b2d377f49da0523ab040724065b02c9753
DanielSalis <danielsalis98@gmail.com>
2022-08-13 11:05:40 -0300

    feat: add threeshake and assets variables

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,4 +53,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    treeShake: true
+  }
 }
