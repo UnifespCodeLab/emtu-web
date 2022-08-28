cec0ab4d812b5b759ecb4bffde7e7ebd03318668
DanielSalis <danielsalis98@gmail.com>
2022-08-28 12:18:58 -0300

    feat: create test

---
+++ pages/__tests__/index.test.js
@@ -0,0 +1,38 @@
+import { mount, createLocalVue } from "@vue/test-utils"
+import Vuetify from 'vuetify'
+
+import SearchPage from "~/pages/index.vue"
+
+describe ("Pages / SearchPage", ()=>{
+  const defaultData={
+    cities: ['São José dos Campos', 'Jacareí', 'Taubaté'],
+    date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+      .toISOString()
+      .substr(0, 10),
+    modalDate: false,
+    time: `${new Date().getHours()}:${new Date().getMinutes()}`,
+    modalTime: false,
+  }
+
+  const localVue = createLocalVue()
+  let vuetify
+
+  beforeEach(() => {
+    vuetify = new Vuetify()
+  })
+
+  describe("when all data is correct", ()=>{
+    it("should render the page", ()=>{
+      const wrapper = mount(SearchPage, {
+        localVue,
+        vuetify,
+        data() {
+          return defaultData
+        }
+      })
+      expect(wrapper.findAll('input').length).toBe(8)
+      expect(wrapper.find('.search-page__image').exists()).toBe(true)
+      expect(wrapper.element).toMatchSnapshot()
+    })
+  })
+})
