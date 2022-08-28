0e3579e8e4f2dd3c073618c3e1e2bad0dd8fe139
DanielSalis <danielsalis98@gmail.com>
2022-08-28 12:17:36 -0300

    chore: jest globals

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,35 @@ module.exports = {
   plugins: [
     'vue',
   ],
+  globals: {
+    //
+    // Tests variables
+    //
+    afterAll: 'readonly',
+    afterEach: 'readonly',
+    beforeAll: 'readonly',
+    beforeEach: 'readonly',
+    describe: 'readonly',
+    xdescribe: 'readonly',
+    expect: 'readonly',
+    it: 'readonly',
+    xit: 'readonly',
+    jest: 'readonly',
+    cy: 'readonly',
+    context: 'readonly',
+    before: 'readonly',
+    //
+    // bdd-lazy-vars
+    //
+    def: 'readonly',
+    get: 'readonly',
+    sharedExamplesFor: 'readonly',
+    includeExamplesFor: 'readonly',
+    itBehavesLike: 'readonly',
+    subject: 'readonly',
+    fdescribe: 'readonly',
+    fit: 'readonly',
+  },
   // add your custom rules here
   rules: {},
 }
