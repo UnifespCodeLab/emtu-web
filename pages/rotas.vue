f7544f934631b0b7473d6adf559bd3e75bd603a8
DanielSalis <danielsalis98@gmail.com>
2022-09-03 10:39:48 -0300

    feat: create page

---
+++ pages/rotas.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="routes-page__container">
+    <span>Pagina Inicial</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RoutesPage",
+}
+</script>
+
+<style lang="scss">
+  .routes-page__container {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 0 20px;
+    height: 100%;
+  }
+</style>
