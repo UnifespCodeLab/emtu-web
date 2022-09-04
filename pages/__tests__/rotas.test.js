b776512fa678c18a740c083991ed6ee641d45f33
DanielSalis <danielsalis98@gmail.com>
2022-09-04 10:34:02 -0300

    feat: creating tests

---
+++ pages/__tests__/rotas.test.js
@@ -0,0 +1,31 @@
+import { mount, createLocalVue } from "@vue/test-utils"
+import Vuetify from 'vuetify'
+
+import RoutesPage from "~/pages/rotas.vue"
+
+describe ("Pages / RoutesPage", ()=>{
+  const defaultData={
+    cidsRoutes: null,
+    model: 0
+  }
+
+  const localVue = createLocalVue()
+  let vuetify
+
+  beforeEach(() => {
+    vuetify = new Vuetify()
+  })
+
+  describe("when all data is correct", ()=>{
+    it("should render the page", ()=>{
+      const wrapper = mount(RoutesPage, {
+        localVue,
+        vuetify,
+        data() {
+          return defaultData
+        }
+      })
+      expect(wrapper.element).toMatchSnapshot()
+    })
+  })
+})
