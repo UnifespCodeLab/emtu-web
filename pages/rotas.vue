f7ea16871c979d7406b9d81c6f07036f4032be8e
DanielSalis <danielsalis98@gmail.com>
2022-09-17 16:30:20 -0300

    fix: milena`s changes

--- pages/rotas.vue
+++ pages/rotas.vue
@@ -7,11 +7,11 @@
         class="routes-page__chip"
         close
         close-icon="mdi-close"
-        :color="cidsRoutes[index].color"
+        :color="item.color"
         text-color="white"
-        @click="()=>updateSelectedCidRoute(index)"
+        @click="updateSelectedCidRoute(index)"
       >
-        {{index}}
+        {{ index }}
       </v-chip>
     </div>
     <div class="routes-page__steps">
@@ -22,7 +22,9 @@
         >
           <v-card class="routes-page__card" height="90%">
             <v-card-text>
-              <div class="font-weight-bold ml-8 mb-2">Itinerário {{ index + 1 }}</div>
+              <div class="font-weight-bold ml-8 mb-2">
+                Itinerário {{ index + 1 }}
+              </div>
 
               <v-timeline align-top dense>
                 <v-timeline-item
@@ -74,21 +76,21 @@ export default {
     },
 
     randomColor() {
-      return this.colors[Math.floor(Math.random() *  this.colors.length)];
-    }
+      return this.colors[Math.floor(Math.random() * this.colors.length)]
+    },
   },
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .routes-page__container {
-  align-items: center;
   display: flex;
   flex-direction: column;
+  align-items: center;
   justify-content: flex-start;
+  height: 100%;
   margin-top: 16px;
   padding: 0 5px;
-  height: 100%;
 
   @media (min-width: 800px) {
     justify-content: center;
@@ -100,9 +102,9 @@ export default {
 }
 
 .routes-page__steps {
+  width: 360px;
   margin-top: 16px;
   margin-bottom: 16px;
-  width: 360px;
 
   @media (min-width: 800px) {
     width: 600px;
