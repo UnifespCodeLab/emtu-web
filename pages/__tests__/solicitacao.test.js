98ebcd7ca0c43802a175ddcf9721ff510cd383b1
Milena Siqueira <milematoss@hotmail.com>
2022-09-21 22:48:16 -0300

    test: add tests for report page

---
+++ pages/__tests__/solicitacao.test.js
@@ -0,0 +1,53 @@
+import { mount, createLocalVue, RouterLinkStub } from '@vue/test-utils'
+import Vuetify from 'vuetify'
+
+import ReportPage from '~/pages/solicitacao'
+
+describe('Pages / ReportPage', () => {
+  const localVue = createLocalVue();
+  let vuetify;
+  let wrapper;
+
+  beforeEach(() => {
+    vuetify = new Vuetify();
+    
+    wrapper = mount(ReportPage, {
+      localVue,
+      vuetify,
+      stubs: {
+        'router-link': RouterLinkStub,
+      },
+    });
+  });
+
+  it('should render the page', () => {
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it('should render correct elements', () => {
+    const alert = wrapper.findComponent({ name: 'v-alert' });
+    expect(alert.props('value')).toBe(false);
+
+    const homeButton = wrapper.findAllComponents({ name: 'v-btn' }).at(1);
+    expect(homeButton.text()).toBe('enviar');
+  });
+
+  describe('when user clicks in submit', () => {
+    beforeEach(() => {
+      const submitButton = wrapper.findAllComponents({ name: 'v-btn' }).at(1);
+      submitButton.trigger('click');
+    });
+
+    it('should render updated page', () => {
+      expect(wrapper.element).toMatchSnapshot();
+    });
+
+    it('should render correct elements', () => {
+      const alert = wrapper.findComponent({ name: 'v-alert' });
+      const homeButton = wrapper.findAllComponents({ name: 'v-btn' }).at(1);
+      
+      expect(alert.props('value')).toBe(true);
+      expect(homeButton.text()).toBe('voltar para home');
+    });
+  });
+})
