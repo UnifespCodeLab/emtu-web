95d8339c4a68f42a7d0431595473b2fafc68db70
DanielSalis <danielsalis98@gmail.com>
2022-10-01 18:48:48 -0300

    feat: add date and chart components

--- pages/admin/acessos-diarios.vue
+++ pages/admin/acessos-diarios.vue
@@ -1,13 +1,181 @@
 <template>
-  <div class="teste">
-    teste
+  <div class="daily-access">
+    <div class="daily-access__time-container">
+      <v-dialog
+        ref="dialogStartDate"
+        v-model="startDateDialog"
+        :return-value.sync="startDate"
+        persistent
+        width="290px"
+      >
+        <template #activator="{ on, attrs }">
+          <v-text-field
+            v-model="startDate"
+            class="daily-acces__input"
+            clearable
+            placeholder="Período de início"
+            label="Período de início"
+            v-bind="attrs"
+            solo
+            v-on="on"
+          />
+        </template>
+        <v-date-picker v-model="startDate" scrollable>
+          <v-spacer />
+          <v-btn text color="primary" @click="startDateDialog = false">
+            Cancelar
+          </v-btn>
+          <v-btn text color="primary" @click="$refs.dialogStartDate.save(startDate)">
+            OK
+          </v-btn>
+        </v-date-picker>
+      </v-dialog>
+      <v-dialog
+        ref="dialogEndDate"
+        v-model="endDateDialog"
+        :return-value.sync="endDate"
+        persistent
+        width="290px"
+      >
+        <template #activator="{ on, attrs }">
+          <v-text-field
+            v-model="endDate"
+            class="daily-acces__input"
+            clearable
+            placeholder="Período final"
+            label="Período final"
+            readonly
+            v-bind="attrs"
+            solo
+            v-on="on"
+          />
+        </template>
+        <v-date-picker v-model="endDate" scrollable>
+          <v-spacer />
+          <v-btn text color="primary" @click="endDateDialog = false">
+            Cancelar
+          </v-btn>
+          <v-btn text color="primary" @click="$refs.dialogEndDate.save(endDate)">
+            OK
+          </v-btn>
+        </v-date-picker>
+      </v-dialog>
+      <v-btn color="primary" large>
+        BUSCAR
+      </v-btn>
+    </div>
+    <div class="daily-access__page-wrapper">
+      <VueApexCharts
+        :options="chartOptions"
+        :series="series"
+      />
+    </div>
   </div>
 </template>
 <script>
+export default {
+  name: 'AdminDailyAccess',
+  components: {
+    VueApexCharts: () => import('vue-apexcharts')
+  },
+  layout: 'admin',
+  data () {
+    return {
+      startDate: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+        .toISOString()
+        .substr(0, 10),
+      startDateDialog: false,
+      endDate: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+        .toISOString()
+        .substr(0, 10),
+      endDateDialog: false,
 
-</script>
-<style scoped>
-  .teste{
-    width: 10px;
+      // CHART OPTIONS
+      series: [{
+        name: 'Desktops',
+        data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+      }],
+      chartOptions: {
+        type: 'line',
+        chart: {
+          type: 'line',
+          zoom: {
+            enabled: false
+          }
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          curve: 'straight'
+        },
+        title: {
+          text: 'Média de acessos diários por mês',
+          align: 'left'
+        },
+        grid: {
+          row: {
+            colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+            opacity: 0.5
+          }
+        },
+        xaxis: {
+          categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep']
+        }
+      }
+    }
   }
+}
+</script>
+<style scoped lang="scss">
+.daily-access {
+  align-items: flex-start;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-left: 4px;
+  padding-right: 4px;
+
+  @media (min-width: 1000px) {
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+}
+.daily-access__time-container {
+  align-items: flex-start;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  justify-content: flex-start;
+  margin-top: 28px;
+  width: 100%;
+
+  @media (min-width: 800px) {
+    align-items: flex-start;
+    display: flex;
+    flex-direction: row;
+    gap: 16px;
+    width: 500px;
+  }
+}
+
+.daily-acces__input {
+  width: 100%;
+  height: 36px;
+
+  @media (min-width: 800px) {
+    width: 188px;
+  }
+}
+
+.daily-access__page-wrapper {
+  margin-top: 60px;
+  width: 100%;
+
+  @media (min-width: 800px) {
+    margin-left: auto;
+    margin-right: auto;
+    width: 800px;
+  }
+}
 </style>
