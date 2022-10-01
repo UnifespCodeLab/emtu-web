84ecdc891cf1a2daed492aba05a156aa48474971
DanielSalis <danielsalis98@gmail.com>
2022-10-01 12:01:55 -0300

    chore: create new admin layout

---
+++ layouts/admin.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <div class="admin-layout">
+      <TheHeader />
+      <div class="admin-layout__content">
+        <Nuxt />
+      </div>
+    </div>
+  </v-app>
+</template>
+
+<script>
+import TheHeader from '~/components/TheHeader.vue'
+
+export default {
+  name: 'AdminLayout',
+  components: {
+    TheHeader
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.admin-layout {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.admin-layout__content {
+  flex: 1;
+  background-color: #e5e5e5;
+}
+</style>
