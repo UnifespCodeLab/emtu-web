cf3c262720cce69da170bbca8d3ef7ae2d7b58ef
DanielSalis <danielsalis98@gmail.com>
2023-01-21 19:42:47 -0300

    feat: improve local stratergies

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,18 +43,20 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:3333/api'
+    baseURL: 'http://localhost:3333/'
   },
 
   auth: {
     strategies: {
       local: {
-        endpoints: {
-          login: { url: '/auth/login', method: 'post', propertyName: 'token' },
-          logout: { url: '/auth/logout', method: 'post' },
-          user: { url: '/user', method: 'get', propertyName: 'data' }
+        token: {
+          required: false,
+          type: false
         },
-        tokenType: 'bearer'
+        endpoints: {
+          login: { url: '/user/login', method: 'post', propertyName: 'token' },
+          user: false
+        }
       }
     }
   },
