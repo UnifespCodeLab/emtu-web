84d2a91ba245e97f2c7aadada0833105f881a9e0
Thiago Henrique Leite da Silva <thiago.leitesilva05@gmail.com>
2023-04-21 22:25:11 -0300

    feat(index): calls emtu-api to save searches in db

--- pages/index.vue
+++ pages/index.vue
@@ -90,7 +90,7 @@
 
 <script>
 import { mapState, mapActions } from 'vuex'
-// import emtuApi from 'assets/services/emtu-api'
+import emtuApi from '~/assets/services/emtu-api'
 
 export default {
   name: 'SearchPage',
@@ -99,9 +99,9 @@ export default {
       modalDate: false,
       modalTime: false,
       searchBody: {
-        originCity: '',
-        destinationCity: '',
-        cid: '',
+        originCity: null,
+        destinationCity: null,
+        cid: null,
         date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
           .toISOString()
           .substr(0, 10),
@@ -125,10 +125,26 @@ export default {
   methods: {
     ...mapActions('city', ['fetchCities']),
     ...mapActions('cid', ['fetchCids']),
-    performSearch () {
+    searchParams () {
+      const { cid, date, destinationCity, originCity, time } = this.searchBody
+
+      return {
+        idCidadeOrigem: originCity,
+        idCidadeDestino: destinationCity,
+        idCid: cid,
+        dataViagem: date,
+        horaViagem: time
+      }
+    },
+    async performSearch () {
       this.$router.push('/rotas')
-      console.log(this.searchBody)
-      // const response = await emtuApi.post('search', this.searchBody)
+
+      try {
+        await emtuApi.post('searches', this.searchParams())
+      } catch (_err) {
+        // eslint-disable-next-line no-console
+        console.error('Falha ao salvar busca no banco de dados!')
+      }
     }
   }
 }
