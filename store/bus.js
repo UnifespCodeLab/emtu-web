30a9e44dabfe13f7305fcdf5b21427443ce0aab2
Milena Siqueira <milematoss1@gmail.com>
2023-04-26 21:58:57 -0300

    feat: create bus store

---
+++ store/bus.js
@@ -0,0 +1,41 @@
+import emtuApi from 'assets/services/emtu-api'
+
+export const state = () => ({
+  busRoutes: []
+})
+
+export const mutations = {
+  setBusRoutes (state, value) {
+    state.busRoutes = value
+  }
+}
+
+export const actions = {
+  async fetchBusRoutes ({ commit }, searchBody) {
+    try {
+      const { data } = await emtuApi.post('/bus', searchBody)
+
+      const formattedResult = []
+
+      data.forEach((line) => {
+        line.vehicle.forEach(({ group }) => {
+          if (!formattedResult.find(item => item.group === group)) {
+            formattedResult.push({ group, routes: [line] })
+          } else {
+            const groupIndex = formattedResult.findIndex(item => item.group === group)
+
+            if (!formattedResult[groupIndex].routes.find(groupedLine => groupedLine.code === line.code)) {
+              formattedResult[groupIndex].routes.push(line)
+            }
+          }
+        })
+      })
+
+      commit('setBusRoutes', formattedResult)
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error)
+      commit('setBusRoutes', null)
+    }
+  }
+}
