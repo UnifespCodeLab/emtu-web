1228d9e392c594265fd9293e21e2124b1a250296
Thiago Henrique <thiago.leite@unifesp.br>
2023-05-03 08:41:30 -0300

    feat(solicitacao): integra página com o back-end (#64)

--- pages/solicitacao.vue
+++ pages/solicitacao.vue
@@ -5,7 +5,7 @@
       dismissible
       transition="scale-transition"
       :type="hasSuccess ? 'success' : 'error'"
-      :value="submittedRoute"
+      :value="showAlert"
     >
       {{ alertMessage }}
     </v-alert>
@@ -23,21 +23,21 @@
       </v-btn>
 
       <template v-else>
-        <v-text-field v-model="requestRouteBody.email" label="E-mail" solo />
+        <v-text-field v-model="reportData.email" label="E-mail" solo />
         <v-autocomplete
-          v-model="requestRouteBody.originCity"
+          v-model="reportData.originCityId"
           :items="cities"
           label="Cidade de origem"
           solo
         />
         <v-autocomplete
-          v-model="requestRouteBody.destinationCity"
+          v-model="reportData.destinationCityId"
           :items="cities"
           label="Cidade de destino"
           solo
         />
         <v-select
-          v-model="requestRouteBody.cid"
+          v-model="reportData.cidId"
           :items="cids"
           label="Cid"
           solo
@@ -59,19 +59,21 @@
 
 <script>
 import { mapState, mapActions } from 'vuex'
-// import emtuApi from 'assets/services/emtu-api'
+import emtuApi from '~/assets/services/emtu-api'
 
 export default {
   name: 'ReportPage',
   data () {
     return {
       hasSuccess: false,
-      submittedRoute: false,
-      requestRouteBody: {
-        email: '',
-        originCity: '',
-        destinationCity: '',
-        cid: ''
+      showAlert: false,
+      errorMessage: '',
+      successMessage: 'Solicitação enviada com sucesso!',
+      reportData: {
+        email: null,
+        originCityId: null,
+        destinationCityId: null,
+        cidId: null
       }
     }
   },
@@ -79,9 +81,17 @@ export default {
     ...mapState('city', ['cities']),
     ...mapState('cid', ['cids']),
     alertMessage () {
-      return this.hasSuccess
-        ? 'Enviado com sucesso!'
-        : 'Erro ao enviar. Tente mais tarde.'
+      return this.hasSuccess ? this.successMessage : this.errorMessage
+    },
+    reportParams () {
+      const { email, originCityId, destinationCityId, cidId } = this.reportData
+
+      return {
+        email,
+        idCidadeOrigem: originCityId,
+        idCidadeDestino: destinationCityId,
+        idCid: cidId
+      }
     }
   },
   created () {
@@ -95,12 +105,18 @@ export default {
   methods: {
     ...mapActions('city', ['fetchCities']),
     ...mapActions('cid', ['fetchCids']),
-    submit () {
-      console.log(this.requestRouteBody)
-      // const response = await emtuApi.post('requestRoute')
+    async submit () {
+      this.showAlert = false
 
-      this.submittedRoute = true
-      this.hasSuccess = true
+      try {
+        await emtuApi.post('reports', this.reportParams)
+        this.hasSuccess = true
+      } catch (error) {
+        this.errorMessage = error?.response?.data?.message || 'Ocorreu um erro ao enviar a solicitação'
+        this.hasSuccess = false
+      } finally {
+        this.showAlert = true
+      }
     }
   }
 }
