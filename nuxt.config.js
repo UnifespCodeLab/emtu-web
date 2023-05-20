a8e0ccf4cd841dca72e0514ad79de7788f8994ab
Danilo Alexandre <danilo.alexandre@sponte.com.br>
2023-05-20 14:26:44 -0300

    adding port from .env

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
   server: {
-    port: 3000 // default: 3000
+    port: process.env.PORT || 3000 // default: 3000
   },
 
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
