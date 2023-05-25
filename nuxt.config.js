64949ac92aab356eee55f6b69ee59861c3d842f5
Danilo Alexandre <danilo.alexandre@sponte.com.br>
2023-05-25 14:47:41 -0300

    ...

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,7 +48,13 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: process.env.EMTU_API || 'http://localhost:3333/'
+    baseURL: process.env.EMTU_API || process.env.NUXT_ENV_EMTU_API || 'http://localhost:3333/'
+  },
+
+  publicRuntimeConfig: {
+    axios: {
+      baseURL: process.env.EMTU_API || process.env.NUXT_ENV_EMTU_API || 'http://localhost:3333/'
+    }
   },
 
   auth: {
