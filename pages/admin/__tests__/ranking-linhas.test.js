f6d123bb3652c0b678f7c23deebe0c4655d3b456
Milena Siqueira <milematoss1@gmail.com>
2023-11-03 23:25:20 -0300

    test: update ranking page tests

--- pages/admin/__tests__/ranking-linhas.test.js
+++ pages/admin/__tests__/ranking-linhas.test.js
@@ -1,15 +1,45 @@
 import { mount, createLocalVue, RouterLinkStub } from '@vue/test-utils'
 import Vuetify from 'vuetify'
+import Vuex, { Store } from 'vuex'
+import emtuApi from '~/assets/services/emtu-api'
 
 import AdminLineRanking from '~/pages/admin/ranking-linhas.vue'
 
+jest.requireActual('~/assets/services/emtu-api')
+
 const localVue = createLocalVue()
+localVue.use(Vuex)
+
 let vuetify
 let wrapper
 class ResizeObserver {
   observe () {}
   unobserve () {}
 }
+
+const endDate = new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+  .toISOString()
+  .substr(0, 10)
+
+const store = new Store({
+  modules: {
+    cid: {
+      namespaced: true,
+      state: {
+        cids: []
+      },
+      actions: {
+        fetchCids: () => []
+      }
+    },
+    alert: {
+      namespaced: true,
+      actions: {
+        showAlert: jest.fn()
+      }
+    }
+  }
+})
 
 describe('Pages / Admin / AdminLineRanking', () => {
   beforeEach(() => {
@@ -21,11 +51,7 @@ describe('Pages / Admin / AdminLineRanking', () => {
     wrapper = mount(AdminLineRanking, {
       localVue,
       vuetify,
-      data () {
-        return {
-          categories: ['1', '2', '3']
-        }
-      },
+      store,
       stubs: {
         'router-link': RouterLinkStub
       }
@@ -59,10 +85,6 @@ describe('Pages / Admin / AdminLineRanking', () => {
   })
 
   it('should render the correct end date', () => {
-    const endDate = new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
-      .toISOString()
-      .substr(0, 10)
-
     const textField = wrapper.findAllComponents({ name: 'v-text-field' }).at(2)
     expect(textField.props('value')).toBe(endDate)
   })
@@ -74,20 +96,49 @@ describe('Pages / Admin / AdminLineRanking', () => {
 
   describe('when search button is clicked', () => {
     beforeEach(() => {
+      emtuApi.get = jest.fn().mockResolvedValue({
+        data: {
+          data: [{
+            idLinha: '244',
+            searchCount: 10
+          },
+          {
+            idLinha: '420',
+            searchCount: 5
+          }]
+        }
+      })
+
       const searchButton = wrapper.findAllComponents({ name: 'v-btn' }).at(0)
       searchButton.trigger('click')
     })
 
-    it('should render the chart container', () => {
+    it('calls emtu-api to execute ranking search', () => {
+      expect(emtuApi.get).toHaveBeenCalledWith('/searches/ranking', {
+        params: {
+          sucedida: null,
+          startDate: null,
+          endDate,
+          idCid: null,
+          limite: null
+        }
+      })
+    })
+
+    it('should render the chart container', async () => {
+      await wrapper.vm.$nextTick()
+
       const container = wrapper.find('.line-ranking__graph-wrapper')
       expect(container.exists()).toBe(true)
     })
 
-    it('should render correct chart', () => {
+    it('should render correct chart', async () => {
+      await wrapper.vm.$nextTick()
+
       const chart = wrapper.findComponent({ name: 'AdminChart' })
       expect(chart.props('title')).toBe('Ranking de linhas')
       expect(chart.props('type')).toBe('bar')
-      expect(chart.props('categories')).toEqual(['1', '2', '3'])
+      expect(chart.props('categories')).toEqual(['244', '420'])
     })
   })
 })
