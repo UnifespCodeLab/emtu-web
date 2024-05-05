1c0f856cd3b68ad3ec9360da7dad1d26dd08e3c4
João Pedro Zampoli <107592828+JoaoPedroZampoli@users.noreply.github.com>
2024-05-04 21:38:59 -0300

    Update TheHeader.test.js

--- components/__tests__/TheHeader.test.js
+++ components/__tests__/TheHeader.test.js
@@ -1,48 +1,47 @@
-import { mount, createLocalVue, RouterLinkStub } from '@vue/test-utils'
+import { mount, createLocalVue } from '@vue/test-utils'
 import Vuetify from 'vuetify'
 
 import TheHeader from '~/components/TheHeader.vue'
 
 describe('Components / TheHeader', () => {
-    const localVue = createLocalVue()
-    let vuetify
-    let wrapper
+  const localVue = createLocalVue()
+  let vuetify
+  let wrapper
 
-    beforeEach(() => {
-        vuetify = new Vuetify()
+  beforeEach(() => {
+      vuetify = new Vuetify()
 
-        wrapper = mount(TheHeader, {
-            localVue,
-            vuetify,
-            mocks: {
-              $route: { }
-            }
-        })
-    })
+      wrapper = mount(TheHeader, {
+        localVue,
+        vuetify,
+        mocks: {
+          $route: { }
+        }
+      })
+  })
 
-    it('matches the screenshot', () => {
-        expect(wrapper.element).toMatchSnapshot()
-    })
+  it('matches the screenshot', () => {
+    expect(wrapper.element).toMatchSnapshot()
+  })
 
-    it('should render title correctly', () => {
-        const title = wrapper.findComponent({ name: 'v-toolbar-title' })
-        expect(title.text()).toBe('EMTU')
-    })
+  it('should render title correctly', () => {
+    const title = wrapper.findComponent({ name: 'v-toolbar-title' })
+    expect(title.text()).toBe('EMTU')
+  })
 
-    it('should render bus icon correctly', () => {
-        const logo = wrapper.find('#bus-icon')
-        expect(logo.exists()).toBe(true)
-    })
+  it('should render bus icon correctly', () => {
+    const logo = wrapper.find('#bus-icon')
+    expect(logo.exists()).toBe(true)
+  })
 
-    it('should render menu icon in the toolbar', () => {
-        const menu = wrapper.find('#menu-icon')
-        expect(menu.exists()).toBe(true)
-    })
+  it('should render menu icon in the toolbar', () => {
+    const menu = wrapper.find('#menu-icon')
+    expect(menu.exists()).toBe(true)
+  })
 
-    it('should toggle SideBar event when menu-icon is clicked', async () => {
-        const menu = wrapper.find('#menu-icon')
-        await menu.trigger('click')
-        expect(wrapper.emitted().toggleSideBar).toBeTruthy()
-    })
-
+  it('should toggle SideBar event when menu-icon is clicked', async () => {
+    const menu = wrapper.find('#menu-icon')
+    await menu.trigger('click')
+    expect(wrapper.emitted().toggleSideBar).toBeTruthy()
+  })
 })
