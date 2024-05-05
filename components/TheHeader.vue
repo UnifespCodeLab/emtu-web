4eee63241c68d28383daee7f8cab8bb1f3d72a4a
João Pedro Zampoli <jpszampoli53@gmail.com>
2024-05-04 21:16:41 -0300

    Criação de Teste do componente TheHeader

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,13 +1,13 @@
 <template>
   <v-card flat height="50px" tile>
     <v-toolbar color="primary" dense dark>
-      <v-icon>mdi-bus</v-icon>
+      <v-icon id="bus-icon">mdi-bus</v-icon>
       <v-toolbar-title class="ml-4">
         EMTU
       </v-toolbar-title>
 
       <v-spacer />
-      <v-app-bar-nav-icon @click="$emit('toggleSideBar')" />
+      <v-app-bar-nav-icon id="menu-icon" @click="$emit('toggleSideBar')" />
     </v-toolbar>
   </v-card>
 </template>
