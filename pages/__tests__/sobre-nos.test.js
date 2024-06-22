2a7bd74c43eafcf8213a275ffbf64f69a2538e62
Thomas Lincoln <thmas5952@gmail.com>
2024-06-22 16:03:03 -0300

    fix: :ambulance: remove teste do carrossel
    remove o teste do carrossel que já não faz mais parte da página

--- pages/__tests__/sobre-nos.test.js
+++ pages/__tests__/sobre-nos.test.js
@@ -26,10 +26,6 @@ describe('Pages / AboutUsPage', () => {
     expect(wrapper.element).toMatchSnapshot()
   })
 
-  it('has a carousel', () => {
-    expect(wrapper.findComponent({ name: 'v-carousel' }).exists()).toBe(true)
-  })
-
   it('has a logo', () => {
     const logo = wrapper.findComponent({ name: 'v-icon' }, { ref: 'mdi-bus' })
     expect(logo.exists()).toBe(true)
