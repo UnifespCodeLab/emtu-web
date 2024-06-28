69a45598da7b8468b3931e81c973cc7f9577582b
liviacanuto <canutto.livia@gmail.com>
2024-06-28 13:01:26 -0300

    test: add the footer component tests

---
+++ components/__tests__/TheFooter.test.js
@@ -0,0 +1,52 @@
+import { mount, createLocalVue, RouterLinkStub } from '@vue/test-utils'
+import Vuetify from 'vuetify'
+
+import TheFooter from '~/components/TheFooter.vue'
+
+describe('Pages / TheFooter', () => {
+  const localVue = createLocalVue()
+
+  let vuetify
+  let wrapper
+
+  beforeEach(() => {
+    vuetify = new Vuetify()
+
+    wrapper = mount(TheFooter, {
+      localVue,
+      vuetify,
+      stubs: {
+        RouterLink : RouterLinkStub
+      }
+    })
+  })
+
+  it('matches the screenshot', () => {
+    expect(wrapper.element).toMatchSnapshot()
+  })
+
+  it('should render buttons correctly', () => {
+    const button = wrapper.findAllComponents('.v-btn')
+    expect(button).toHaveLength(4)
+  })
+
+  it('should render linkedin icon correctly', () => {
+    const logo = wrapper.find('.mdi-linkedin')
+    expect(logo.exists()).toBe(true)
+  })
+
+  it('should render email icon correctly', () => {
+    const logo = wrapper.find('.mdi-email')
+    expect(logo.exists()).toBe(true)
+  })
+
+  it('should render github icon correctly', () => {
+    const logo = wrapper.find('.mdi-github')
+    expect(logo.exists()).toBe(true)
+  })
+
+  it('should render text button correctly', () => {
+    const text = wrapper.text()
+    expect(text).toBe('Não encontrou sua rota? Nos informe aqui!')
+  })
+})
